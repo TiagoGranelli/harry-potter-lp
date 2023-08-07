53ed4a8ca4365caa3b29f85fd45d1a4b848f35b1
TiagoGranelli <tiago.granelli@gmail.com>
2023-08-07 19:03:03 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import "./App.scss";
+import CharacterCard from "./components/CharacterCard/CharacterCard";
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://hp-api.onrender.com/api/characters")
+      .then((response) => response.json())
+      .then((data) => setCharacters(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Harry Potter Characters</h1>
+      <div className="characters-container">
+        {characters.map((character) => (
+          <CharacterCard key={character.name} character={character} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
