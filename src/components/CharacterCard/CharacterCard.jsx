d4beb01744b46b8e1b6d9ba5f7aeda0639a9c9db
TiagoGranelli <tiago.granelli@gmail.com>
2023-08-07 19:42:32 -0300

    feat: only show wizards characters

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import "./CharacterCard.scss";
 
 function CharacterCard({ character }) {
+  if (!character.wizard) {
+    return null;
+  }
+
   return (
     <div className="character-card">
       <img src={character.image || "/fallback.jpg"} alt={character.name} />
       <h2>{character.name}</h2>
-      <p>Date of Birth: {character.dateOfBirth}</p>
-      <p>House: {character.house}</p>
-      <p>Patronus: {character.patronus}</p>
+      <p>Date of Birth: {character.dateOfBirth || "Unknown"}</p>
+      <p>House: {character.house || "Unknown"}</p>
+      <p>Patronus: {character.patronus || "Don't have"}</p>
       <p>Actor: {character.actor}</p>
-      <p>Status: {character.alive ? "Alive" : "Deceased"}</p>
+      <p>Character status: {character.alive ? "Alive" : "Deceased"}</p>
     </div>
   );
 }
